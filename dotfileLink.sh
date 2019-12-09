56618236d810ca3d5801487a7b3807de30a2bd72
Bernkastel07th <koki.sato.work@gmail.com>
2019-12-09 10:16:20 +0900

    feat: シンボリックリンク zshrc

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -3,6 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vim ~/
 ln -sf ~/dotfiles/.powerline-shell.json ~/.powerline-shell.json
 ln -snfv ${HOME}/.vim ${HOME}/.config/nvim/
