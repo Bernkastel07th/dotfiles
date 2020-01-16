48dd99954f5a6fb6e0c12a4325bdde093e2fdfad
Bernkastel07th <koki.sato.work@gmail.com>
2020-01-16 16:29:08 +0900

    feat: add .zsh_profile

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
