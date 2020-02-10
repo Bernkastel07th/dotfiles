766ba1bc6045bdc57fa9d884a384d6db3ee8a1a5
Bernkastel07th <koki.sato.work@gmail.com>
2020-02-10 17:00:41 +0900

    feat: add fish && fix dotfilesLink.sh

--- dotfileLink.sh
+++ dotfileLink.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zsh_profile ~/.zsh_profile
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vim ~/
-ln -sf ~/dotfiles/.powerline-shell.json ~/.powerline-shell.json
-ln -snfv ${HOME}/.vim/* ${HOME}/.config/nvim
-ln -snfv ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
-ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
+ROOT_DIR=~/dotfiles
+ln -sfv $ROOT_DIR/.vimrc ~/.vimrc
+ln -sfv $ROOT_DIR/.bash_profile ~/.bash_profile
+ln -sfv $ROOT_DIR/.zsh_profile ~/.zsh_profile
+ln -sfv $ROOT_DIR/.tmux.conf ~/.tmux.conf
+ln -sfv $ROOT_DIR/.bashrc ~/.bashrc
+ln -sfv $ROOT_DIR/.zshrc ~/.zshrc
+ln -sfv $ROOT_DIR/.vim ~/
+ln -sfv $ROOT_DIR/.powerline-shell.json ~/.powerline-shell.json
+ln -sfv $ROOT_DIR/.gitconfig ~/.gitconfig
+ln -sfv $ROOT_DIR/fish ~/.config/fish
+ln -sfv ${HOME}/.vim ~/.config/nvim
+ln -snv ${HOME}/.vimrc ~/.config/nvim/init.vim
