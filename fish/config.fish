15cd9302728b0fc673c1d48621e0fd9a5ac99d4a
Bernkastel07th <koki.sato.work@gmail.com>
2020-02-10 18:38:26 +0900

    feat: update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,22 @@
 set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+
+function peco_select_history_order
+  if test (count $argv) = 0
+    set peco_flags --layout=top-down
+  else
+    set peco_flags --layout=bottom-up --query "$argv"
+  end
+
+  history|peco $peco_flags|read foo
+
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
+
+function fish_user_key_bindings
+  bind /cr 'peco_select_history_order' # Ctrl + R
+end
