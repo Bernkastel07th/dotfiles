4b11258525134b39278ab0d63f5b7b84a8e6075d
Bernkastel07th <koki.sato.work@gmail.com>
2020-02-12 16:55:52 +0900

    feat: fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,17 @@
+# --------------------
+# FZF config
+# --------------------
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
+
+# --------------------
+# peco config
+# --------------------
+set fish_plugins theme peco
+
+function fish_user_key_bindings
+  bind \cr peco_select_history # Bind for prco history to Ctrl+r
+end
 
 function peco_select_history_order
   if test (count $argv) = 0
@@ -17,6 +29,19 @@ function peco_select_history_order
   end
 end
 
+# --------------------
+# bobthefish config
+# --------------------
+set -g fish_prompt_pwd_dir_length 0  # ディレクトリ省略しない
+set -g theme_newline_cursor yes  # プロンプトを改行した先に設ける
+set -g theme_display_git_master_branch yes  # git の branch 名を表示
+set -g theme_color_scheme dracula
+set -g theme_display_date no  # 時刻を表示しないように設定
+set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
+
+# --------------------
+# keymapping
+# --------------------
 function fish_user_key_bindings
   bind /cr 'peco_select_history_order' # Ctrl + R
 end
